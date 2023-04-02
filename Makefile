0eaf97bc4d1c26872bf29094d97672f0485eb372
Nikhil Sasidharan <sasidharann@vmware.com>
2023-04-02 09:12:19 +0800

    Initial Commit

---
+++ Makefile
@@ -0,0 +1,35 @@
+GPPPARAMS = -m32 -fno-use-cxa-atexit -nostdlib -fno-builtin -fno-rtti -fno-exceptions -fno-leading-underscore
+ASPARAMS = --32
+LDPARAMS = -melf_i386
+
+objects = loader.o kernel.o
+
+%.o: %.cpp # create obj files from cpp files
+	g++ $(GPPPARAMS) -o $@ -c $<
+
+%.o: %.s # create obj files from as files
+	as $(ASPARAMS) -o $@ $<
+
+foldKernel.bin: linker.ld $(objects) # link as and cpp to create kernel binary
+	ld $(LDPARAMS) -T  $< -o $@ $(objects)
+
+install: foldKernel.bin
+	sudo cp $< /boot/foldKernel.bin
+
+foldKernel.iso: foldKernel.bin
+	mkdir iso
+	mkdir iso/boot
+	mkdir iso/boot/grub
+	cp $< iso/boot
+	echo 'set timeout=0' > iso/boot/grub/grub.cfg
+	echo 'set default=0' >> iso/boot/grub/grub.cfg
+	echo '' >> iso/boot/grub/grub.cfg
+	echo 'menuentry "Fold OS" {' >> iso/boot/grub/grub.cfg
+	echo '  multiboot /boot/foldKernel.bin' >> iso/boot/grub/grub.cfg
+	echo '  boot' >> iso/boot/grub/grub.cfg
+	echo '}' >> iso/boot/grub/grub.cfg
+	grub-mkrescue --output=$@ iso
+	rm -rf iso
+
+run: foldKernel.iso
+	VirtualBoxVM --startvm "Fold" &
---
+++ kernel.cpp
@@ -0,0 +1,55 @@
+/*  get the pointer to a memory location, 
+	leave the high byte as is, combine that with string to be printed
+*/
+void printf(char* str) {
+	unsigned short* VideoMemory = (unsigned short*)0xb8000;
+	for(int i = 0; str[i] != '\0'; ++i)
+		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+}
+
+typedef void (*constructor)();         // declaring a function pointer
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors()
+{
+	for(constructor* i = &start_ctors; i != &end_ctors; i++)
+		(*i)();
+}
+
+/* bootloader has stored the multiboot structure in ram, 
+   which we should accept as params to our main function
+*/
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicNumber) {
+	printf("Hello MAC!!");
+
+	while(1);
+}
+
+
+/*	VIDEO1:
+1:  First, need assembly to set the "stack pointer" and then 
+    invoke the kernel mail function, as C++ expects it
+2:  The grub will not recognize our bin file as a kernel 
+    unless we add the "multiboot sector" and the magic number
+3:  The multiboot structure will be stored somewhere in RAM for 
+    later use and address stored in ax register
+    and the magic value in bx register.
+    We will use this later to get the total RAM size
+4.  Loader fill will compile but the kernel will not, 
+	as printf function needs to be defined
+5.  Tell the compiler there is no glibc, no exception handler
+	no mem management
+6.	Time for linker. KernelMain needs to have extern C
+
+	VIDEO2:
+1.	add start_ctors in kernel file, to invoke the global objects
+	constructors and put between start_ctors and end_ctors
+
+	VIDEO3:
+1.	creating iso target in makefile
+	means adding grub-rescue and virtualbox
+	got error: grub-mkrescue: error: mformat invocation failed
+	solution: apt install mtools 
+*/
+
+
